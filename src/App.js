0374cf8e0d40c0918035ae974254a016d4d7fa80
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-01-24 07:38:10 +0300

    Basic Redux Data Flow

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Fragment } from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
+
+import { Navbar } from './app/Navbar'
+import { PostsList } from './features/posts/PostsList'
+import { AddPostForm } from './features/posts/AddPostForm'
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div className="App">
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <Fragment>
+                <AddPostForm />
+                <PostsList />
+              </Fragment>
+            )}
+          />
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+export default App
